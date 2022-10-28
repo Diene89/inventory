edf3d277b6e6bc8e9235b966b128d1453da70f83
Diene89 <diene_89@hotmail.com>
2022-10-28 15:34:05 -0300

    feat: Testa o construtor/inicializador do objeto Produto

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,5 +1,21 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_cria_produto():
-    pass  # Seu teste deve ser escrito aqui
+    new_product = Product({
+        "id": 9,
+        "nome_do_produto": "eucalyptus globulus",
+        "nome_da_empresa": "Target Corporation",
+        "data_de_fabricacao": "2020-09-06",
+        "data_de_validade": "2024-05-21",
+        "numero_de_serie": "GT74 LHWJ FCXL JNQT ZCXM 4761 GWSP",
+        "instrucoes_de_armazenamento": "instrucao 9"
+    })
+
+    assert new_product.id == 9
+    assert new_product.nome_do_produto == "eucalyptus globulus"
+    assert new_product.nome_da_empresa == "Target Corporation"
+    assert new_product.data_de_fabricacao == "2020-09-06"
+    assert new_product.data_de_validade == "2024-05-21"
+    assert new_product.numero_de_serie == "GT74 LHWJ FCXL JNQT ZCXM 4761 GWSP"
+    assert new_product.instrucoes_de_armazenamento == "instrucao 9"
