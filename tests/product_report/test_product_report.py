320d0f509dc996acbe9ac852caaf08324547eb5e
Diene89 <diene_89@hotmail.com>
2022-10-28 16:28:39 -0300

    feat: testa o relatório individual do produto

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,18 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    product = Product(
+        "9",
+        "eucalyptus globulus",
+        "Target Corporation",
+        "2020-09-06",
+        "2024-05-21",
+        "GT74 LHWJ FCXL JNQT ZCXM 4761 GWSP",
+        "ao abrigo da luz",
+    )
+    assert (
+        str(product) == "O produto eucalyptus globulus fabricado em 2020-09-06"
+        " por Target Corporation com validade até 2024-05-21"
+        " precisa ser armazenado ao abrigo da luz."
+    )
