c7940ea5b1537a0a41f902ddf82820789eafd6ef
Diene89 <diene_89@hotmail.com>
2022-10-28 21:35:54 -0300

    Apaga comentarios de código

--- inventory_report/reports/complete_report.py
+++ inventory_report/reports/complete_report.py
@@ -2,32 +2,6 @@ from collections import Counter
 
 from inventory_report.reports.simple_report import SimpleReport
 
-
-# class CompleteReport(SimpleReport):
-#     @classmethod
-#     def get_products_quantity_by_company(self, stock):
-
-#         companies = Counter(
-#             product["nome_da_empresa"] for product in stock
-#         ).most_common()
-#         complete_stock = ""
-
-#         for company, quantity in companies:
-#             complete_stock += f"- {company}: {quantity}\n"
-
-#         return complete_stock
-
-#     @classmethod
-#     def generate(self, stock):
-#         report = SimpleReport.generate(stock)
-#         products_list = self.get_products_quantity_by_company(self, stock)
-
-#         return (
-#             f"{report}\n"
-#             f"Produtos estocados por empresa:\n"
-#             f"{products_list}"
-#         )
-
 class CompleteReport():
     def get_products_quantity_by_company(self, stock):
         count_companies = list()
